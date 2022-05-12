0877d82999a7d0e2f59c4d8c7eb3eb577894e512
IJustMa <46496195+KarloGraf@users.noreply.github.com>
2022-05-13 00:55:01 +0200

    Added most of the Customer Main activity elements
    Added the frontend of the Customer main activity and added skeleton functions for most of the elements.

---
+++ app/src/main/java/com/example/shoestoreapp/customer/RecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.shoestoreapp.customer;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.shoestoreapp.R;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecycleViewAdapter";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<Float> ratings = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mNames, ArrayList<String> mImageUrls, ArrayList<Float> ratings) {
+        this.mNames = mNames;
+        this.mImageUrls = mImageUrls;
+        this.ratings = ratings;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mContext).
+                asBitmap().
+                load(mImageUrls.get(position))
+                .into(holder.productImage);
+        holder.productName.setText(mNames.get(position));
+        holder.productRating.setRating(ratings.get(position));
+
+        holder.productImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO item onclick
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView productImage;
+        TextView productName;
+        RatingBar productRating;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            productImage = itemView.findViewById(R.id.imageViewItem);
+            productName = itemView.findViewById(R.id.textViewItemName);
+            productRating = itemView.findViewById(R.id.ratingBar);
+
+        }
+    }
+}
