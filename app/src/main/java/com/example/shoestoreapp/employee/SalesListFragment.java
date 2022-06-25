8b493643f31a6d33968f2dafb7c5cddc4c8b01e4
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-06-25 21:31:20 +0200

    Fixed date picker color.

--- app/src/main/java/com/example/shoestoreapp/employee/SalesListFragment.java
+++ app/src/main/java/com/example/shoestoreapp/employee/SalesListFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
