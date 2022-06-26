edbe8f802beeec2b69fc801d5295e7f257ae53ac
Vanja Ljubobratovic <vanja1708@gmail.com>
2022-06-26 15:03:07 +0200

    Removed google sign in code.

--- app/src/main/java/com/example/shoestoreapp/LoginActivity.java
+++ app/src/main/java/com/example/shoestoreapp/LoginActivity.java
@@ -26,21 +26,11 @@ import com.example.shoestoreapp.admin.AdminMainActivity;
 import com.example.shoestoreapp.customer.CustomerMainActivity;
 import com.example.shoestoreapp.databinding.ActivityLoginBinding;
 import com.example.shoestoreapp.employee.EmployeeMainActivity;
-import com.firebase.ui.auth.data.model.User;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -54,17 +44,12 @@ import java.util.ArrayList;
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private ProgressDialog loadingBar;
-    private static final int RC_SIGN_IN = 100;
-
-    private GoogleSignInClient googleSignInClient;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore database;
     private CollectionReference usersRef;
     private UserModel user;
     private SharedPreferences sharedPref;
-
-    private static final String TAG ="GOOGLE_SIGN_IN";
 
 
 
@@ -80,21 +65,8 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-                .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
-
-        binding.googleSignInBtn.setOnClickListener(view -> {
-            Log.d(TAG, "BEGIN SIGN IN");
-            Intent intent = googleSignInClient.getSignInIntent();
-            startActivityForResult(intent, RC_SIGN_IN);
-        });
 
         binding.loginBtn.setEnabled(true);
         binding.loginBtn.setOnClickListener(view -> {
@@ -413,56 +385,5 @@ public class LoginActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == RC_SIGN_IN) {
-            Log.d(TAG, "onActivityResult: Google Sign In intent Result");
-            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = accountTask.getResult(ApiException.class);
-                firebaseAuthWithGoogleAccount(account);
-            } catch (Exception e) {
-                Log.d(TAG, "onActivityResult: Error " + e.getMessage());
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogleAccount(GoogleSignInAccount account) {
-        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth");
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        firebaseAuth.signInWithCredential(credential)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        Log.d(TAG, "onSuccess: Logged in");
-                        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                        String uid = firebaseUser.getUid();
-                        String email = firebaseUser.getEmail();
-
-                        Log.d(TAG, "onSuccess: Email " + email);
-                        Log.d(TAG, "onSuccess: UID " + uid);
-                        
-                        //check if user is new
-                        if(authResult.getAdditionalUserInfo().isNewUser()) {
-                            Log.d(TAG, "onSuccess: Account Created... " + email);
-                            Toast.makeText(LoginActivity.this, "Account Created... \n" + email, Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.d(TAG, "onSuccess: Existing user " + email);
-                        }
-
-                        startActivity(new Intent(LoginActivity.this, LoggedInActivity.class));
-                        finish();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "onSuccess: Login failed " + e.getMessage());
-                    }
-                });
     }
 }
